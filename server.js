7768a1a575cba06cf5c2f589f6efde83beee2be0
bherendeen <her19051@byui.edu>
2023-01-23 15:59:35 -0700

    JAN 23 2023 - 3:59pm

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // addon mods
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 // custom mods
 const mongodb = require('./db/connection');
 // run express
@@ -10,11 +11,19 @@ const app = express();
 // port
 const port = process.env.PORT || 8080;
 
+// -------------------- //
+
+const swaggerDocument = require('./swagger.json');
+
 // code
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
